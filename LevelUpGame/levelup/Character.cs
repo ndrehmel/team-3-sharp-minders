400f06895eaae8c011aa8a5280d6bc98829847a4
NitinG30 <nitingwalani@yahoo.com>
2023-01-26 20:08:18 +0000

    Adding Set and Get Position for Character.cs

--- LevelUpGame/levelup/Character.cs
+++ LevelUpGame/levelup/Character.cs
@@ -1,8 +1,11 @@
+using System.Drawing;
+
 namespace levelup{
 public class Character {
 
     public string? Name{get; set;}
     public int? MoveCount {get; set;}
+    public Point Position (0,0);
 
     public Character(){
         this.Name = "";
@@ -13,5 +16,14 @@ public class Character {
         this.Name = name;
         this.MoveCount = 0;
     }
+
+    public Point SetPosition(Point pos){
+        Position.X = pos.X;
+        Position.Y = pos.Y;
+    }
+
+    public string GetPosition(){
+        return "Character " + this.Name + " is on {" + Position.X.ToString() + "," + Position.Y.ToString() + "} and your Move Count is :" + MoveCount.ToString();
+    } 
 }
 }
