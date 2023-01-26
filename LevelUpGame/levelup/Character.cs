1653090df3c31e485121c045b0883f6be4d17520
NitinG30 <nitingwalani@yahoo.com>
2023-01-26 19:18:38 +0000

    Add Character class

---
+++ LevelUpGame/levelup/Character.cs
@@ -0,0 +1,15 @@
+public Class Character {
+
+    public string? Name{get; set;}
+    public int? MoveCount {get; set;}
+
+    public Character(){
+        this.Name = "";
+        this.MoveCount = 0;
+    }
+
+    public Character(string name){
+        this.Name = name;
+        this.MoveCount = 0;
+    }
+}
