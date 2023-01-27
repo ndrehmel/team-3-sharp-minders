00614fe12f6d55922af80547dfb1f0e057f4e6cb
ndrehmel <nathaniel.drehmel@gmail.com>
2023-01-27 14:42:11 +0000

    updates to gamemap

--- LevelUpGame/levelup/GameMap.cs
+++ LevelUpGame/levelup/GameMap.cs
@@ -4,9 +4,20 @@ namespace levelup
 {
     public class GameMap
     {
+        //public Point currentPosition = new Point(0,0);
+
         public GameMap()
         {
             
+        }
+
+        public Point calculatePosition(Point currentPosition, GameController.DIRECTION direction)
+        {
+            if (GameController.DIRECTION.NORTH == direction)
+            {
+                currentPosition.Y += 1;
+            }
+            return currentPosition;
         }
     }
 }
